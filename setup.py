b3891bf07ed302a955092d631cefc1b000719b37
Trieu Vu <trieuv@strategicdata.com.au>
2020-09-03 16:35:32 +1000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
 setup(
-    name="bitbucket CLI",
-    version="0.1",
+    name="bitbucket-cli",
+    version="1",
     py_modules=["repo"],
     packages=["lib", "lib.bitbucketCLI"],
     include_package_data=True,
     install_requires=["click", "requests"],
+    url="https://github.com/thvu11/Bitbucket-CLI.git",
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: Python Software Foundation License"
+    ],
     entry_points="""
         [console_scripts]
         bb=lib.bb:cli
